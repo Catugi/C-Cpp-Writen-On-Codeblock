cd858bcaa7cadb3992f8029b37802d887bab67ee
Catugi <99402289+Catugi@users.noreply.github.com>
2022-03-13 12:08:46 +0100

    Update main.cpp

--- ranges/main.cpp
+++ ranges/main.cpp
@@ -1,9 +1,12 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
+#include <ranges>
 
 
 auto main()->int
 {
     std::string nome ="Fernando";
+    std::ranges::sort(nome);
     std::cout<<nome<<std::endl;
 }
